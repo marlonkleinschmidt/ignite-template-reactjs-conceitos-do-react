6ba3aa126efa8cff63a573cba2ddb54c028094bc
Your Name <you@example.com>
2022-05-16 12:58:35 -0300

    challenge chapterI

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,36 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    
+    if(!newTaskTitle){
+      return;
+    }
+
+    const addTask = {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    setTasks([...tasks, addTask]);
+    
   }
 
-  function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+  function handleToggleTaskCompletion(id: number) {    
+        
+    const alterTask = tasks.map(
+      (task) => task.id === id 
+      ? {
+          ...task, 
+          isComplete: !task.isComplete 
+        } 
+      : task);
+
+    setTasks(alterTask);
   }
 
-  function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+  function handleRemoveTask(id: number) {    
+    const removeTaskById = tasks.filter((task) => task.id !== id);
+    setTasks(removeTaskById);
   }
 
   return (
