b0910464d9adc173df9c2adc453ea29038420e62
Your Name <you@example.com>
2022-08-08 17:14:29 -0300

    fix:layout

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -50,12 +50,12 @@ export function TaskList() {
   return (
     <section className="task-list container">
       <header>
-        <h2>Minhas tasks</h2>
+        <h2>Minha listagem</h2>
 
         <div className="input-group">
           <input 
             type="text" 
-            placeholder="Adicionar novo todo" 
+            placeholder="Adicionar novo item..." 
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
